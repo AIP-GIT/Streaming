24bfe9b486fa664add581ee3e2f5b9801b665efe
Amit <aiperceptron.tube@gmail.com>
2022-03-23 19:32:59 +0530

    +pyspark Basics

---
+++ PySpark-Basics/envsetup.sh
@@ -0,0 +1,5 @@
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export JRE_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre
+# export SPARK_HOME='/home/ubuntu-poc/workspace/projects/Streaming/PySpark/spark-2.1.0-bin-hadoop2.7'
+export SPARK_HOME='/home/ubuntu-poc/workspace/projects/Streaming/POC-PySpark/spark-3.2.1-bin-hadoop3.2'
+
